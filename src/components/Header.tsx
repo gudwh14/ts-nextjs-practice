6acb7d479120e1ef89adcb2efd104a2dae6966c0
gudwh14 <alxious@naver.com>
2021-07-14 15:10:21 +0900

    refactor : 스태틱 파일 참조하기

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,11 @@
 const Header = () => {
     return (
         <div>
-            <img src="/public/images/jjo_ico.jpeg" alt="아이콘"/>
+            {/**
+                static 파일을 참조 할때는 /public 경로를 붙이지않고 바로 /images 를 참조하면 된다.
+                public 폴더를 여러개 할경우 에러가 발생함. public 폴더 이름 변경 불가
+            **/}
+            <img src="/images/jjo_ico.jpeg" alt="아이콘"/>
             <h1>JJo</h1>
         </div>
     );
