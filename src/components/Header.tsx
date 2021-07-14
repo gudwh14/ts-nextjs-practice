02794dd763c1f97c68716a193328ea9ec93afe35
gudwh14 <alxious@naver.com>
2021-07-14 15:04:13 +0900

    feat : 공통 컴포넌트 Header 생성

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+    return (
+        <div>
+            <img src="/public/images/jjo_ico.jpeg" alt="아이콘"/>
+            <h1>JJo</h1>
+        </div>
+    );
+};
+
+export default Header;
