ba5146f64631114814efee1afc8de9e5531dc810
gudwh14 <alxious@naver.com>
2021-07-14 15:35:08 +0900

    feat : next/Head 사용

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,9 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
     return (
         <div>
+            <Head>
+                <title>HOME | JJo</title>
+            </Head>
             <h1>Create-Next-App 을 이용해 설치하게 되면</h1>
             <p>1. 컴파일과 번들링이 자동으로 된다. (webpack, Babel)</p>
             <p>2. 자동 리프레쉬 기능을 지원하여 수정시 바로 반영된다.</p>
